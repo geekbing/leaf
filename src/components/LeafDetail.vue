638d99bcfe116cb33d3ff553f45820f41e928c02
zhengbinbin <zhengbinbin@58ganji.com>
2017-08-18 18:45:28 +0800

    新增查看文章详情页

---
+++ src/components/LeafDetail.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="leafDetail-container" v-cloak v-loading.body="pageLoading" element-loading-text="加载中...">
+    <el-row>
+      <el-col>
+        <p class="article-title">{{article.title}}</p>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <p class="article-second-title"><span>{{article.username}}</span><span
+          class="blank"></span><span>{{article.createTime | dateFormatter}}</span></p>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <p class="leaf-tags">
+          <el-tag :type="index % 2 === 0 ? '' : 'gray'" class="el-tag-random" v-for="(tag, index) in tags" :key="index">
+            {{tag}}
+          </el-tag>
+        </p>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <div class="article-body">{{article.body}}</div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  import fetch from '../utils/fetch'
+  import { dateFormatter } from '../utils/util'
+
+  export default {
+    name: 'leafDetail',
+    data: function () {
+      return {
+        pageLoading: false,
+        article: {
+          title: '',
+          username: '',
+          createTime: '',
+          body: ''
+        },
+        tags: []
+      }
+    },
+    created: function () {
+      this.fetchData()
+    },
+    filters: {
+      dateFormatter (time) {
+        return dateFormatter(time)
+      }
+    },
+    methods: {
+      fetchData () {
+        let vm = this
+        vm.pageLoading = true
+        fetch({
+          method: 'get',
+          url: '/leafapi/article/' + vm.$route.params.id
+        }).then(function (response) {
+          if (response.status === 200) {
+            vm.article = response.data.data.article
+            vm.tags = response.data.data.tags
+            vm.pageLoading = false
+          }
+        })
+      },
+      handleRegister () {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  [v-cloak] {
+    display: none !important;
+  }
+
+  .leafDetail-container {
+    width: 100%;
+    height: 100%;
+  }
+
+  .article-title {
+    font-size: 20px;
+    color: #666;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  .article-second-title {
+    text-align: center;
+    font-size: 14px;
+    color: #999;
+  }
+
+  .blank {
+    margin: 6px;
+  }
+
+  .leaf-tags {
+    text-align: center;
+  }
+
+  .el-tag-random {
+    margin-right: 8px;
+  }
+
+  .el-tag-random:hover {
+    cursor: pointer;
+  }
+
+  .article-body {
+    margin: auto 50px;
+  }
+</style>
