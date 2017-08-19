44cff096eda18b3b73083e95fde011373fc90276
Bing <dhuzbb@163.com>
2017-08-19 09:34:00 +0800

    对el-tag进行封装，增加随机颜色功能

---
+++ src/components/Color-Tag.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-tag :type="tagType" class="color-tag">
+    <slot></slot>
+  </el-tag>
+</template>
+
+<script>
+  export default {
+    name: 'color-tag',
+    computed: {
+      tagType () {
+        let types = ['', 'gray', 'primary', 'success', 'warning', 'danger']
+        return types[Math.floor(Math.random() * 6)]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .color-tag {
+    margin-right: 8px;
+  }
+</style>
